61b62c05e0a4399d890e89262e79cf0039d524a7
davep3rrett <david.m.perrett@gmail.com>
2015-07-24 01:48:17 -0400

    finish tweeting logic, add oauth stuff

--- bot.rb
+++ bot.rb
@@ -24,5 +24,28 @@ class Bot
     end
     return tweets.sample
 	end
+
+  # Return twitter username associated with the tweet
+  def get_username(tweet)
+    return tweet.attrs[:user][:screen_name]
+  end
+
+  def build_response(username, quote)
+    tweet = "@#{username} #{quote}"
+    if tweet.length > 140
+      return tweet[0,140]
+    else
+      return tweet
+    end
+  end
+
+  def reload_quotes(file)
+    puts "clearing and reloading quotes..."
+    @quotes.clear
+    File.open(file, 'r').each do |line|
+      @quotes.push(line)
+    end
+    puts "quotes successfully reloaded."
+  end
   
 end
--- main.rb
+++ main.rb
@@ -1,20 +1,35 @@
 #!/usr/bin/env ruby
 
+require 'oauth'
 require 'twitter'
 require './bot.rb'
 
-# Set up the bot
+consumer = OAuth::Consumer.new(ENV["CONSUMER_KEY"], ENV["CONSUMER_SECRET"], {:site => "https://api.twitter.com", :scheme => :header})
+
+request_token = consumer.get_request_token(:oauth_callback => :oob)
+puts "Visit this URL: #{request_token.authorize_url}"
+print "Enter the PIN: "
+
+access_token = request_token.get_access_token(:oauth_verifier => STDIN.gets.chomp)
+
+ACCESS_TOKEN = access_token.token
+ACCESS_SECRET = access_token.secret
+
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["CONSUMER_KEY"]
   config.consumer_secret     = ENV["CONSUMER_SECRET"]
-  config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_SECRET"]
+  config.access_token        = ACCESS_TOKEN
+  config.access_token_secret = ACCESS_SECRET
 end
 
-filename = ARGV[0]
+quotes_file = ARGV[0]
 
-bot = Bot.new(filename, client)
+bot = Bot.new(quotes_file, client)
 
-# Try out a couple methods
-puts bot.find_tweet("#chillin").text
-puts bot.random_quote
+while(true)
+  bot.reload_quotes(quotes_file)
+  tweet = bot.find_tweet("#chillin")
+  response = bot.build_response(bot.get_username(tweet), bot.random_quote)
+  client.update(response)
+  sleep(30 * 60)
+end
