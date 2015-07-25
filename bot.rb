b55b22b7f1fa76a64c3a60d4821c7cc0f9c70075
davep3rrett <david.m.perrett@gmail.com>
2015-07-25 00:44:07 -0400

    fix indentation

--- bot.rb
+++ bot.rb
@@ -1,29 +1,29 @@
 #!/usr/bin/env ruby
 
 class Bot
-	def initialize(file, client)
-		@quotes = Array.new
-		File.open(file, 'r').each do |line|
-			@quotes.push(line)
-		end
-		@client = client		
-	end
+  def initialize(file, client)
+    @quotes = Array.new
+    File.open(file, 'r').each do |line|
+      @quotes.push(line)
+    end
+    @client = client		
+  end
 
   # Return a random element from the quotes array
-	def random_quote
-		return @quotes.sample
-	end
+  def random_quote
+    return @quotes.sample
+  end
 	
   # Store recent tweets matching search string
   # in an array, and return a random element of
   # the array
-	def find_tweet(search_string)
+  def find_tweet(search_string)
     tweets = Array.new
-		@client.search(search_string, result_type: "recent").take(10).each do |tweet|
+    @client.search(search_string, result_type: "recent").take(10).each do |tweet|
       tweets.push(tweet)
     end
     return tweets.sample
-	end
+  end
 
   # Return twitter username associated with the tweet
   def get_username(tweet)
@@ -47,5 +47,5 @@ class Bot
     end
     puts "quotes successfully reloaded."
   end
-  
+
 end
